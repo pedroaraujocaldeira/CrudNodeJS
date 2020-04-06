f8dffede3386ec6f490857cd31b070a438ea30e0
Pedro Henrique <pedrohac1993@gmail.com>
2020-04-05 22:34:39 -0300

    adicionando procfile

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
 const cors = require('cors');
-
-
+const env = require('env');
 
 // docker
 // init docker "docker start <name>"
@@ -15,7 +14,8 @@ app.use(express.json());
 app.use(cors());
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi');
+
+mongoose.connect(env.MONGO_URL);
 requireDir('./src/models');
 
 // Rotas
